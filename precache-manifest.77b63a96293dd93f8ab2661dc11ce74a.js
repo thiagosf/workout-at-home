860de09d98e98d41da2d006f4ec06716f3d67688
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2020-07-12 17:48:36 -0300

    Updates

---
+++ precache-manifest.77b63a96293dd93f8ab2661dc11ce74a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fc1f32c2f0defcc3c5f425641f3ed8",
+    "url": "/workout-at-home/index.html"
+  },
+  {
+    "revision": "57b47d2572be1fc5c240",
+    "url": "/workout-at-home/static/css/2.9be4f162.chunk.css"
+  },
+  {
+    "revision": "45b23eb7def888717636",
+    "url": "/workout-at-home/static/css/main.c933e478.chunk.css"
+  },
+  {
+    "revision": "57b47d2572be1fc5c240",
+    "url": "/workout-at-home/static/js/2.8061d079.chunk.js"
+  },
+  {
+    "revision": "0385a72e796212aaf82fc019fd275845",
+    "url": "/workout-at-home/static/js/2.8061d079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5174c9e9c6a29ef45ba6",
+    "url": "/workout-at-home/static/js/3.e4c899c8.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/workout-at-home/static/js/3.e4c899c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b23eb7def888717636",
+    "url": "/workout-at-home/static/js/main.9de3d7a6.chunk.js"
+  },
+  {
+    "revision": "cd7eb37c84cac3d209d5",
+    "url": "/workout-at-home/static/js/runtime-main.7eb6d28f.js"
+  },
+  {
+    "revision": "a6bf9b86a07f7e0c5d7348432dbc40e8",
+    "url": "/workout-at-home/static/media/end-time.a6bf9b86.mp3"
+  },
+  {
+    "revision": "8887d961a9f3da59b6ff0ffd13e9c663",
+    "url": "/workout-at-home/static/media/logo-dark.8887d961.svg"
+  },
+  {
+    "revision": "bf99de1da70135fe7a784a7e0ccbbbe7",
+    "url": "/workout-at-home/static/media/logo-light.bf99de1d.svg"
+  },
+  {
+    "revision": "85dcea10b44653e32b362605a2a2091f",
+    "url": "/workout-at-home/static/media/logo-small.85dcea10.svg"
+  },
+  {
+    "revision": "3d26505c1a6ef55a28e7ed02caed9496",
+    "url": "/workout-at-home/static/media/next-exercise.3d26505c.mp3"
+  },
+  {
+    "revision": "e1ed54a8e88a51176d1c6c349ef09fa9",
+    "url": "/workout-at-home/static/media/onboarding-1.e1ed54a8.svg"
+  },
+  {
+    "revision": "d928424e05365848d9d078f3d3e78462",
+    "url": "/workout-at-home/static/media/onboarding-2.d928424e.svg"
+  },
+  {
+    "revision": "fe0523a0ce2c2eb83e55c12a51e54d88",
+    "url": "/workout-at-home/static/media/onboarding-3.fe0523a0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/workout-at-home/precache-manifest.6dc61174d408edd75300072dc89fdec9.js"
+  "/workout-at-home/precache-manifest.77b63a96293dd93f8ab2661dc11ce74a.js"
 );
 
 self.addEventListener('message', (event) => {
